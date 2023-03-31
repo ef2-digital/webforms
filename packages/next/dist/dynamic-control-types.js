5c0f1ec0e4ea1dd424f2c70484467874ab7cb2ae
Daan de Gooijer <daan@ef2.nl>
2023-03-31 13:34:43 +0200

    removed dist

--- packages/next/dist/dynamic-control-types.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=dynamic-control-types.js.map
--- packages/next/dist/index.js
+++
@@ -1,4 +0,0 @@
-import WebForm from "./WebForm";
-import getTranslator from "./translations/translator";
-export { WebForm, getTranslator };
-//# sourceMappingURL=index.js.map
